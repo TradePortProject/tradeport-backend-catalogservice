d1fe960ce655f7d49d5b47b65d12f80af54f07c6
prabhu <prabhulalitha48@gmail.com>
2025-10-20 18:30:52 +0800

    Mongo product changes

--- src/main/java/com/tradeport/catalog/model/Product.java
+++ src/main/java/com/tradeport/catalog/model/Product.java
@@ -3,14 +3,28 @@ package com.tradeport.catalog.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @Document(collection = "products")
 public class Product {
     @Id
-    private String id;
-    private String name;
+    private String productID;
+    private String productCode;
+    private String manufacturerID;
+    private String productName;
     private String description;
-    private List<Variant> variants;
+    private String category;
+    private double wholesalePrice;
+    private double retailPrice;
+    private int quantity;
+    private String retailCurrency;
+    private String wholeSaleCurrency;
+    private double shippingCost;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+    private boolean isActive;
+    private List<ProductImage> productImage;
 }
---
+++ src/main/java/com/tradeport/catalog/model/ProductImage.java
@@ -0,0 +1,8 @@
+package com.tradeport.catalog.model;
+
+import lombok.Data;
+
+@Data
+public class ProductImage {
+    private String productImageURL;
+}
