d093a85b82405c6e5644e52b298b760047b462b2
prabhu <prabhulalitha48@gmail.com>
2025-10-22 11:05:19 +0800

    Mango catalog1

--- src/main/java/com/tradeport/catalog/CatalogServiceApplication.java
+++ src/main/java/com/tradeport/catalog/CatalogServiceApplication.java
@@ -3,7 +3,7 @@ package com.tradeport.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 public class CatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
